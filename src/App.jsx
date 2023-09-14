3e9e450c76f029cdd6655b4a2ab3496af14a3576
Oshioke-Salaki <salaki1902@gmail.com>
2023-09-14 12:57:52 -0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+  font-size: 30px;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+`;
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <H1>The wild oasis</H1>
+      <Button></Button>
+    </div>
+  );
 }
 
 export default App;

d190085fbbb295adcd205c5e0cbdcb860ae0242b
Oshioke-Salaki <salaki1902@gmail.com>
2023-09-18 10:22:09 -0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,12 @@
 import styled from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
   background-color: yellow;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: var(--border-radius-sm);
-  background-color: var(--color-brand-600);
-  color: var(--color-brand-50);
-  box-shadow: var(--shadow-sm);
-  cursor: pointer;
-`;
-
-const Input = styled.input`
-  border: 1px solid var(--color-grey-300);
-  background-color: var(--color-brand-0);
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
-  box-shadow: var(--shadow-sm);
 `;
 
 const StyledApp = styled.main`

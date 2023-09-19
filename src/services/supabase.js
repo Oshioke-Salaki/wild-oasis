573673b10d0365319d6b86ff39c77dcbb0dccad4
Oshioke-Salaki <salaki1902@gmail.com>
2023-09-19 13:07:19 -0600

    update

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://eqncnaztuvbiuppjmyjj.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxbmNuYXp0dXZiaXVwcGpteWpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTUwNTQ5MjEsImV4cCI6MjAxMDYzMDkyMX0.1JPNQhqI5moCzZz3E04bwDKzohkoujFQU6wfyaDhHEk';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;

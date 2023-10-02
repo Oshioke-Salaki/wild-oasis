f80b5650a87c2792b914960cf1d0c68d45ba3248
Oshioke-Salaki <salaki1902@gmail.com>
2023-10-01 19:21:02 -0600

    update

--- src/data/data-cabins.js
+++ src/data/data-cabins.js
@@ -1,4 +1,4 @@
-const { supabaseUrl } = require('services/supabase');
+import { supabaseUrl } from '../services/supabase';
 const imageUrl = `${supabaseUrl}/storage/v1/object/public/cabin-images/`;
 
 // Book now and experience luxury in nature.

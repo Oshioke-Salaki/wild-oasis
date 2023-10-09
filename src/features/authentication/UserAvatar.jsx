a17dfbc52e01cbe708adfed7d51a3ad3fcf12be6
Oshioke-Salaki <salaki1902@gmail.com>
2023-10-09 04:03:55 -0600

    update

--- src/features/authentication/UserAvatar.jsx
+++ src/features/authentication/UserAvatar.jsx
@@ -15,7 +15,7 @@ const Avatar = styled.img`
   width: 4rem;
   width: 3.6rem;
   aspect-ratio: 1;
-  /* object-fit: cover; */
+  object-fit: cover;
   object-position: center;
   border-radius: 50%;
   outline: 2px solid var(--color-grey-100);
